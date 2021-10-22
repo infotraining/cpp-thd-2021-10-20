97135ff6ea4fb75a300fdb9480ed2e8ba0cfb777
Krystian Piekos <krystian.piekos@infotraining.pl>
2021-10-22 14:00:59 +0200

    packaged_task & promise

--- _exercises/monte-carlo-pi/pi.cpp
+++ _exercises/monte-carlo-pi/pi.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <random>
+#include <future>
 
 using namespace std;
 
@@ -27,6 +28,26 @@ void bg_pi(size_t N, long &hits)
     }
 
     hits += local_hits;
+}
+
+long calc_hits(size_t N)
+{
+    std::random_device rd;
+    std::mt19937_64 gen{rd()};
+    std::uniform_real_distribution<double> rnd_gen(-1.0, 1.0);
+
+    long local_hits = 0;
+
+    for (long n = 0; n < N; ++n)
+    {
+        double x = rnd_gen(gen);
+        double y = rnd_gen(gen);
+
+        if (x * x + y * y < 1)
+            local_hits++;
+    }
+
+    return local_hits;
 }
 
 
@@ -88,5 +109,35 @@ int main()
 
         cout << "MULTI Pi = " << pi << endl;
         cout << "MULTI Elapsed = " << elapsed_time << "ms" << endl;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    ///
+    ///
+
+    {
+        cout << "Pi calculation started!" << endl;
+        const auto start = chrono::high_resolution_clock::now();
+
+        std::vector<std::future<long>> results;
+
+        for(int i=0; i < n_of_cores; ++i)
+        {
+            std::future<long> f = std::async(std::launch::async, &calc_hits, N_part);
+            results.push_back(std::move(f));
+        }
+
+        long hits = 0L;
+
+        for(auto& h : results)
+            hits += h.get();
+
+        const double pi = static_cast<double>(hits) / N * 4;
+
+        const auto end = chrono::high_resolution_clock::now();
+        const auto elapsed_time = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+        cout << "Future Pi = " << pi << endl;
+        cout << "Future PI Elapsed = " << elapsed_time << "ms" << endl;
     }
 }
--- futures/main.cpp
+++ futures/main.cpp
@@ -34,6 +34,125 @@ void save_to_file(const std::string& filename)
     std::cout << "File saved: " << filename << std::endl;
 }
 
+void wtf()
+{
+    auto f1 = std::async(std::launch::async, &save_to_file, "F1");
+    auto f2 = std::async(std::launch::async, &save_to_file, "F2");
+    auto f3 = std::async(std::launch::async, &save_to_file, "F3");
+    auto f4 = std::async(std::launch::async, &save_to_file, "F4");
+}
+
+int async_demo()
+{
+    std::future<int> f1 = std::async(std::launch::async, &calculate_square, 13);
+    std::future<int> f2 = std::async(std::launch::async, &calculate_square, 9);
+    std::future<void> f3 = std::async(std::launch::async, [] { save_to_file("Some content"); });
+
+    while ( f1.wait_for(100ms) != std::future_status::ready)
+    {
+        std::cout << "I'm still waiting..." << std::endl;
+    }
+
+    int r1 = f1.get();
+    std::cout << "r1: " << r1 << std::endl;
+
+    try
+    {
+        int r2 = f2.get();
+        std::cout << "r2: " << r2 << std::endl;
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cout << "Caught: " << e.what() << std::endl;
+    }
+
+    f3.wait();
+
+    std::cout << "\n--------------------------\n" << std::endl;
+
+    wtf();
+}
+
+template <typename Callable>
+auto launch_async(Callable callable)
+{
+    using ResultT = decltype(callable());
+
+    std::packaged_task<ResultT()> pt{callable};
+    std::future<ResultT> f = pt.get_future();
+
+    std::thread thd{std::move(pt)};
+    thd.detach();
+
+    return f;
+}
+
+
+void packaged_task_demo()
+{
+    std::packaged_task<int()> pt1([] { return calculate_square(13); });
+    std::future<int> f1 = pt1.get_future();
+
+    std::thread thd1{std::move(pt1)};
+    thd1.detach();
+
+    std::cout << "result: " << f1.get() << std::endl;
+
+    auto s1 = launch_async([] { save_to_file("F1");});
+    auto s2 = launch_async([] { save_to_file("F2");});
+    auto f = launch_async([] { return calculate_square(13); });
+
+    std::cout << "result: " << f.get() << std::endl;
+
+    s2.wait();
+}
+
+class SquareCalculator
+{
+    std::promise<int> promise_;
+public:
+    void calculate(int x)
+    {
+        try
+        {
+            int result = calculate_square(x);
+            promise_.set_value(result);
+        }
+        catch (...)
+        {
+            promise_.set_exception(std::current_exception());
+        }
+    }
+
+    std::future<int> get_future()
+    {
+        return promise_.get_future();
+    }
+};
+
+void promise_demo()
+{
+    SquareCalculator calc;
+
+    std::future<int> f = calc.get_future();
+    std::shared_future shared_f = f.share();
+
+    std::thread thd_consumer{[shared_f] {
+            std::cout << "Consuming: " << shared_f.get() << std::endl;
+        }
+    };
+
+    std::thread thd_calc{[&calc] { calc.calculate(17); } };
+
+    std::cout << "17 * 17 = " << shared_f.get() << std::endl;
+
+    thd_calc.join();
+    thd_consumer.join();
+}
+
 int main()
 {
+    //packaged_task_demo();
+
+    promise_demo();
 }
